bcba265d415eda8fec4696c9da8bb3be19484aa2
PragatiVe <pragativerma613@gmail.com>
2026-02-17 12:50:32 +0530

    fix: standard Prisma client setup

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -13,13 +13,19 @@
 // if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 // src/lib/prisma.ts
-import { PrismaClient } from '../generated/prisma';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
 
+const connectionString = process.env.DATABASE_URL!;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
+// prevent multiple instances in dev (Next.js hot reload)
 const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined };
 
 export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient();
+  globalForPrisma.prisma ?? new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma;
